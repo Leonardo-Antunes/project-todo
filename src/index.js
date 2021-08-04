5bd5c3c414fddb94b432f04349622408ffabb84b
alinemartinsgh <aline.p.martins@globalhitss.com.br>
2021-08-04 13:09:09 -0300

    created store and provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
