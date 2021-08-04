597b6d300a69169758fc8969efbad9ceee284ab8
alinemartinsgh <aline.p.martins@globalhitss.com.br>
2021-08-04 12:01:49 -0300

    created input reducer

---
+++ src/redux/reducers/input.js
@@ -0,0 +1,47 @@
+/* eslint-disable import/no-anonymous-default-export */
+import actionTypes from "../actionTypes";
+
+const initialState = {
+  id: -1,
+  title: "",
+  description: "",
+  tag: "todo"
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_INPUT_ID: {
+      const { id } = action;
+      return {
+        ...state,
+        id,
+      }
+    }
+    case actionTypes.SET_INPUT_TITLE: {
+      const { title } = action;
+      return {
+        ...state,
+        title,
+      }
+    }
+    case actionTypes.SET_INPUT_DESCRIPTION: {
+      const { description } = action;
+      return {
+        ...state,
+        description,
+      }
+    }
+    case actionTypes.SET_ITEM_TAG: {
+      const { tag } = action;
+      return {
+        ...state,
+        tag,
+      }
+    }
+    case actionTypes.RESET_INPUT: {
+      return initialState
+    }
+    default:
+      return state;
+  }
+}
