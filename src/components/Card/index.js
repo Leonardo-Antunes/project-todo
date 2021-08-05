cf75bd65c777d50d75f8ddda46eecd99510a806e
Leonardo-Antunes <leonardo.antunes@globalhitss.com.br>
2021-08-05 16:02:22 -0300

    component ready to receive props data

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,9 +4,9 @@ import { CardTitle, Wrapper, CardDescription, CardTag } from "./style";
 const Card = ({ title, description, tag, onItemClicked }) => {
   return (
     <Wrapper role="button" onClick={onItemClicked}>
-      <CardTag> {tag} </CardTag>
       <CardTitle> {title} </CardTitle>
       <CardDescription> {description} </CardDescription>
+      <CardTag> {tag} </CardTag>
     </Wrapper>
   );
 };
--- src/components/SecaoDeTarefas/index.js
+++ src/components/SecaoDeTarefas/index.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import inputActions from "../../redux/actions/inputActions";
 import { Card } from "..";
+import {} from "./style";
 
 const SecaoDeTarefas = () => {
+  const dispatch = useDispatch();
+  const itens = useSelector((state) => state);
+
+  const onItemClicked = (item, index) => {
+    dispatch(inputActions.setInputId(index));
+    dispatch(inputActions.setInputTitle(item.title));
+    dispatch(inputActions.setInputDescription(item.description));
+    // dispatch(inputActions.setItemTag(item.tag));
+  };
+
+  // if (itens.length === 0) {
+  //   return <p>Nao ha todos</p>;
+  // }
+
   return (
-    <Card title="a" description="b" tag="c"></Card>
+    <div>
+      {itens.map((item, index) => {
+        if (item) {
+          return (
+            <Card
+              title={item.title}
+              description={item.description}
+              tag={item.tag}
+              onItemClicked={() => {
+                onItemClicked(item, index);
+              }}
+            />
+          );
+        }
+        return null;
+      })}
+    </div>
   );
 };
 
