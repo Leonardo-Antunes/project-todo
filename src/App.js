1e246642f14fe2cb8f73fdec4814d3d8d84299a9
alinemartinsgh <aline.p.martins@globalhitss.com.br>
2021-08-17 13:56:22 -0300

    add background and other minor styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
 import { SecaoDeTarefas, FormTodo } from "./components";
-import { CssBaseline } from "@material-ui/core";
+import { BgTheme } from "./GlobalTheme";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={BgTheme}>
       <CssBaseline />
       <FormTodo />
       <SecaoDeTarefas />
-    </>
+    </ThemeProvider>
   );
 }
--- src/components/SecaoDeTarefas/style.js
+++ src/components/SecaoDeTarefas/style.js
@@ -13,6 +13,7 @@ export const LottieWrapper = styled(Box)`
 `;
 
 export const BoxTarefas = styled(Box)`
+  width: 22vw;
   background-color: #f8f9fa;
   border-radius: 8px;
 `;
@@ -25,7 +26,9 @@ export const Title = styled(Typography)`
 
 export const LottieTitle = styled(Typography)`
   text-align: center;
+  color: #fff;
   font-weight: 600;
   font-size: 24px;
   padding: 0.5rem;
+  margin-bottom: 1.5rem;
 `;
